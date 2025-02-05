4c4ed34502128bac6a48bbab4ba1e2b3f907b622
alvaroch975 <alvaroch975@gmail.com>
2025-02-05 13:28:19 +0100

    ACTUALIZASAO COMENTARIO

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+        System.out.println("ALEJEN A LUCIA DE LAS TIENDAS DE ANIMALES!");
+        System.out.println("HACE ASADOS!");
     }
 }
